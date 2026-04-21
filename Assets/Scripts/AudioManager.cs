b521f85787e44524cc75509d58e98dac5be534ff
Roman Grechushnikov <259741062+RomanGrechushnikov@users.noreply.github.com>
2026-04-21 23:48:11 +0200

    Setup settings PopUp. Setup Background music

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,102 @@
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance;
+    
+    [Header("Audio Sources")]
+    public AudioSource bgmSource;
+    public AudioSource sfxSource;
+    
+    [Header("Audio Clips")]
+    public AudioClip buttonClick;
+    public AudioClip placement;
+    public AudioClip strike;
+    public AudioClip popupOpen;
+    public AudioClip popupClose;
+    
+    private bool bgmEnabled = true;
+    private bool sfxEnabled = true;
+    
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        
+        LoadSettings();
+        ApplySettings();
+    }
+    
+    void LoadSettings()
+    {
+        bgmEnabled = PlayerPrefs.GetInt("BGMEnabled", 1) == 1;
+        sfxEnabled = PlayerPrefs.GetInt("SFXEnabled", 1) == 1;
+    }
+    
+    void ApplySettings()
+    {
+        if (bgmSource != null)
+            bgmSource.mute = !bgmEnabled;
+        if (sfxSource != null)
+            sfxSource.mute = !sfxEnabled;
+    }
+    
+    public void SetBGM(bool enabled)
+    {
+        bgmEnabled = enabled;
+        PlayerPrefs.SetInt("BGMEnabled", enabled ? 1 : 0);
+        PlayerPrefs.Save();
+        if (bgmSource != null)
+            bgmSource.mute = !enabled;
+    }
+    
+    public void SetSFX(bool enabled)
+    {
+        sfxEnabled = enabled;
+        PlayerPrefs.SetInt("SFXEnabled", enabled ? 1 : 0);
+        PlayerPrefs.Save();
+        if (sfxSource != null)
+            sfxSource.mute = !enabled;
+    }
+    
+    public bool IsBGMEnabled() => bgmEnabled;
+    public bool IsSFXEnabled() => sfxEnabled;
+    
+    public void PlayButtonClick()
+    {
+        if (sfxEnabled && buttonClick != null)
+            sfxSource.PlayOneShot(buttonClick);
+    }
+    
+    public void PlayPlacement()
+    {
+        if (sfxEnabled && placement != null)
+            sfxSource.PlayOneShot(placement);
+    }
+    
+    public void PlayStrike()
+    {
+        if (sfxEnabled && strike != null)
+            sfxSource.PlayOneShot(strike);
+    }
+    
+    public void PlayPopupOpen()
+    {
+        if (sfxEnabled && popupOpen != null)
+            sfxSource.PlayOneShot(popupOpen);
+    }
+    
+    public void PlayPopupClose()
+    {
+        if (sfxEnabled && popupClose != null)
+            sfxSource.PlayOneShot(popupClose);
+    }
+}
---
+++ Assets/Scripts/SettingsManager.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsManager : MonoBehaviour
+{
+    public GameObject settingsPopup;
+    public Toggle bgmToggle;
+    public Toggle sfxToggle;
+    
+    void Start()
+    {
+        settingsPopup.SetActive(false);
+        
+        if (bgmToggle != null)
+        {
+            bgmToggle.isOn = PlayerPrefs.GetInt("BGMEnabled", 1) == 1;
+            bgmToggle.onValueChanged.AddListener(OnBGMToggled);
+        }
+        
+        if (sfxToggle != null)
+        {
+            sfxToggle.isOn = PlayerPrefs.GetInt("SFXEnabled", 1) == 1;
+            sfxToggle.onValueChanged.AddListener(OnSFXToggled);
+        }
+    }
+    
+    public void ShowSettings()
+    {
+        settingsPopup.SetActive(true);
+    }
+    
+    public void HideSettings()
+    {
+        settingsPopup.SetActive(false);
+    }
+    
+    void OnBGMToggled(bool isOn)
+    {
+        PlayerPrefs.SetInt("BGMEnabled", isOn ? 1 : 0);
+        PlayerPrefs.Save();
+        
+        if (AudioManager.Instance != null)
+            AudioManager.Instance.SetBGM(isOn);
+    }
+    
+    void OnSFXToggled(bool isOn)
+    {
+        PlayerPrefs.SetInt("SFXEnabled", isOn ? 1 : 0);
+        PlayerPrefs.Save();
+        
+        if (AudioManager.Instance != null)
+            AudioManager.Instance.SetSFX(isOn);
+    }
+}
